51fab2e69d01ab6b5d87ced11f053aae8e4f07b4
Eli Gooch <eli.gooch@centre.edu>
2019-09-17 10:55:41 -0400

    message

--- javascript.js
+++ javascript.js
@@ -2,4 +2,9 @@ var chooseScooter = function(){
     var choice = document.createElement("th");
     console.log("choice",choice);
     
+    var button = document.querySelector("#scooter");
+        
+    button.setAttribute("onclick",null);
 }
+
+chooseScooter();
